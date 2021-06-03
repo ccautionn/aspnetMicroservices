366504ff550ae95ff1a9a36e0546bd0e878be12a
Ahmet ALMIŞ <ahmetalmis@yahoo.com.tr>
2021-06-03 23:14:33 +0300

    CatalogAPI tamamlandı.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionStrings"));
+            var database = client.GetDatabase("DatabaseSettings:DatabaseName");
+
+            Products = database.GetCollection<Product>("DatabaseSettings:CollectionName");
+
+           CatalogContextSeed.SeedData(Products);
+        }
+
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
