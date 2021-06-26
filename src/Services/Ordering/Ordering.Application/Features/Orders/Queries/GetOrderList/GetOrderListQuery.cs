f5445ffe16ed1d9d8a1d7aa8ef5f3617f7dff78b
ccautionn <ahmetalmis@yahoo.com.tr>
2021-06-26 18:23:10 +0300

    order clean architect and DDD implementation

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQuery : IRequest
+    {
+        public string Username { get; set; }
+
+        public GetOrderListQuery(string username)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+    }
+}
